cb7e92b6268a1020795731f346f1ccf7df7b41b9
Juliana Navarro <98998508+JulianaNavarro@users.noreply.github.com>
2022-04-27 22:37:37 -0300

    Add files via upload

---
+++ Udemy/JS-Completo/curto_circuito.js
@@ -0,0 +1,36 @@
+// CURTO CIRCUITO : 
+
+//let n = 0;
+
+//if(n === 0){
+
+// OU
+
+//    if(!n){
+//   n = 10
+//}
+//  console.log(n)
+
+//  OU
+
+let n = 0
+n = n || 10
+
+console.log(n)
+
+//========================================================//
+
+// let isValid = true
+
+//if (isValid){
+//    console.log(" É válido! ")
+//}
+
+// OU
+
+let isValid = false
+
+isValid && console.log("É válido ")
+
+
+
