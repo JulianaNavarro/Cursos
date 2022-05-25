6427ab463eee3fe5328a2bd63c0c0c92e2f98f3f
Juliana Navarro <98998508+JulianaNavarro@users.noreply.github.com>
2022-05-25 12:30:47 -0300

    Add files via upload

---
+++ Udemy/JS-Completo/objects2.js
@@ -0,0 +1,14 @@
+//interação objects
+
+const pessoa = {
+   nome: "Maria", 
+   idade: 28,
+   "email": "ela@server.com"
+}
+console.log (pessoa)
+
+for (let prop in pessoa) {
+
+   console.log(prop)
+   console.log(pessoa[prop])
+}
